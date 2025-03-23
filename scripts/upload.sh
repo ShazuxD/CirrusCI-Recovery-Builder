dd0960ac4fa93fbc25d7549b38d42f12e1026308
ShazuxD <shazuxd7@gmail.com>
2025-03-23 13:49:56 +0530

    fixes

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+sudo apt update &&sudo apt insta jq -y
 source $CONFIG
 cd /tmp/ci/out/target/product/$DEVICE
 ls
-curl --upload-file $OUTPUT https://free.keep.sh >> upload.txt
+curl -F "file=@${OUTPUT}" https://temp.sh/upload >> upload.txt
 wget https://github.com/Sushrut1101/GoFile-Upload/raw/master/upload.sh >> /dev/null
 chmod +x upload.sh
 sudo apt install jq -y >> /dev/null
